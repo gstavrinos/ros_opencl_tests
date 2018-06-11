32c2e38593acfd8857eece8d60888ae1b0a620f3
George Stavrinos <gstavrinos@iit.demokritos.gr>
2018-06-11 16:35:59 +0300

    Fixed wrong bracket placement due to previous deletions, and a small typo

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -60,19 +60,19 @@ void callback (const sensor_msgs::Image& msg){
         result.data[i] = sum[0] / cnt;
         result.data[i+1] = sum[1] / cnt;
         result.data[i+2] = sum[2] / cnt;
-        pub.publish(result);
     }
+    pub.publish(result);
 }
 
 int main (int argc, char** argv){
     ros::init (argc, argv, "ros_opencl_tests_cpu");
     ros::NodeHandle nh;
-    std::string int_topic;
+    std::string in_topic;
     std::string result_topic;
-    nh.param("ros_opencl_tests_cpu/int_topic", int_topic, std::string("/usb_cam/image_raw"));
+    nh.param("ros_opencl_tests_cpu/int_topic", in_topic, std::string("/usb_cam/image_raw"));
     nh.param("ros_opencl_tests_cpu/result_topic", result_topic, std::string("ros_opencl_tests_cpu/result"));
     pub = nh.advertise<sensor_msgs::Image>(result_topic, 1);
-    ros::Subscriber s = nh.subscribe (int_topic, 1, callback);
+    ros::Subscriber s = nh.subscribe (in_topic, 1, callback);
     while(ros::ok()){
         ros::spin();
     }
