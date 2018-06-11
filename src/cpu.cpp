10b8033ac7568fc67ed3bfe4e4be0b5d02ca7a47
George Stavrinos <gstavrinos@iit.demokritos.gr>
2018-06-11 16:25:55 +0300

    Fixed cpu test

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -3,69 +3,65 @@
 #include <sensor_msgs/Image.h>
 
 ros::Publisher pub;
-sensor_msgs::Image prev_msg;
 
 void callback (const sensor_msgs::Image& msg){
-    if (prev_msg.data.size() > 0){
-        sensor_msgs::Image result = sensor_msgs::Image(msg);
-        for(int i = 0; i < msg.data.size(); i+=3){
-            unsigned sum[3] = {result.data[i], result.data[i+1], result.data[i+2]};
-            unsigned cnt = 1;
-            if((int)(i - msg.step) >= 0){
-                sum[0] += msg.data[i-msg.step];
-                sum[1] += msg.data[i+1-msg.step];
-                sum[2] += msg.data[i+2-msg.step];
-                cnt++;
-            }
-            if((int)(i - msg.step - 3) >= 0){
-                sum[0] += msg.data[i-msg.step-3];
-                sum[1] += msg.data[i+1-msg.step-3];
-                sum[2] += msg.data[i+2-msg.step-3];
-                cnt++;
-            }
-            if((int)(i - msg.step + 3) >= 0){
-                sum[0] += msg.data[i-msg.step+3];
-                sum[1] += msg.data[i+1-msg.step+3];
-                sum[2] += msg.data[i+2-msg.step+3];
-                cnt++;
-            }
-            if((int)(i + msg.step) < msg.data.size()){
-                sum[0] += msg.data[i+msg.step];
-                sum[1] += msg.data[i+1+msg.step];
-                sum[2] += msg.data[i+2+msg.step];
-                cnt++;
-            }
-            if((int)(i + msg.step - 3) < msg.data.size()){
-                sum[0] += msg.data[i+msg.step-3];
-                sum[1] += msg.data[i+1+msg.step-3];
-                sum[2] += msg.data[i+2+msg.step-3];
-                cnt++;
-            }
-            if((int)(i + msg.step + 3) < msg.data.size()){
-                sum[0] += msg.data[i+msg.step+3];
-                sum[1] += msg.data[i+1+msg.step+3];
-                sum[2] += msg.data[i+2+msg.step+3];
-                cnt++;
-            }
-            if((int)(i - 3) >= 0){
-                sum[0] += msg.data[i-3];
-                sum[1] += msg.data[i+1-3];
-                sum[2] += msg.data[i+2-3];
-                cnt++;
-            }
-            if((int)(i + 3) < msg.data.size()){
-                sum[0] += msg.data[i+3];
-                sum[1] += msg.data[i+1+3];
-                sum[2] += msg.data[i+2+3];
-                cnt++;
-            }
-            result.data[i] = sum[0] / cnt;
-            result.data[i+1] = sum[1] / cnt;
-            result.data[i+2] = sum[2] / cnt;
+    sensor_msgs::Image result = sensor_msgs::Image(msg);
+    for(int i = 0; i < msg.data.size(); i += 3){
+        unsigned sum[3] = {result.data[i], result.data[i+1], result.data[i+2]};
+        unsigned cnt = 1;
+        if((int)(i - msg.step) >= 0){
+            sum[0] += msg.data[i-msg.step];
+            sum[1] += msg.data[i+1-msg.step];
+            sum[2] += msg.data[i+2-msg.step];
+            cnt++;
         }
+        if((int)(i - msg.step - 3) >= 0){
+            sum[0] += msg.data[i-msg.step-3];
+            sum[1] += msg.data[i+1-msg.step-3];
+            sum[2] += msg.data[i+2-msg.step-3];
+            cnt++;
+        }
+        if((int)(i - msg.step + 3) >= 0){
+            sum[0] += msg.data[i-msg.step+3];
+            sum[1] += msg.data[i+1-msg.step+3];
+            sum[2] += msg.data[i+2-msg.step+3];
+            cnt++;
+        }
+        if((int)(i + msg.step) < msg.data.size()){
+            sum[0] += msg.data[i+msg.step];
+            sum[1] += msg.data[i+1+msg.step];
+            sum[2] += msg.data[i+2+msg.step];
+            cnt++;
+        }
+        if((int)(i + msg.step - 3) < msg.data.size()){
+            sum[0] += msg.data[i+msg.step-3];
+            sum[1] += msg.data[i+1+msg.step-3];
+            sum[2] += msg.data[i+2+msg.step-3];
+            cnt++;
+        }
+        if((int)(i + msg.step + 3) < msg.data.size()){
+            sum[0] += msg.data[i+msg.step+3];
+            sum[1] += msg.data[i+1+msg.step+3];
+            sum[2] += msg.data[i+2+msg.step+3];
+            cnt++;
+        }
+        if((int)(i - 3) >= 0){
+            sum[0] += msg.data[i-3];
+            sum[1] += msg.data[i+1-3];
+            sum[2] += msg.data[i+2-3];
+            cnt++;
+        }
+        if((int)(i + 3) < msg.data.size()){
+            sum[0] += msg.data[i+3];
+            sum[1] += msg.data[i+1+3];
+            sum[2] += msg.data[i+2+3];
+            cnt++;
+        }
+        result.data[i] = sum[0] / cnt;
+        result.data[i+1] = sum[1] / cnt;
+        result.data[i+2] = sum[2] / cnt;
         pub.publish(result);
     }
-    prev_msg = msg;
 }
 
 int main (int argc, char** argv){
